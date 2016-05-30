b3479f1dac12c9ed4e97612f78f44f3c173f534e
Joel Rocha <joelxr@gmail.com>
2016-05-30 13:55:05 -0300

    PostController

---
+++ web/PostController.js
@@ -0,0 +1,7 @@
+angular.module('descubra', []).controller('PostController', function() {
+    this.posts = [
+            {"id" : 1, "name" : "Doe"},
+            {"id" : 2, "name" : "Smith"}
+        ];
+});
+
